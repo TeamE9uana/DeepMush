71e79bfc1f7c47d502d174e0534a6d464cec0574
jiho lee <optional.int@kakao.com>
2022-01-03 15:08:03 +0900

    feat: add DJANGO_ALLOW_ASYNC_UNSAFE

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from .environments import get_secret
+import os
+
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
