51c051ff3216ddc21e81a420a87bbff718073985
jiho lee <optional.int@kakao.com>
2022-01-03 15:05:30 +0900

    feat: init django project

---
+++ backend/config/environments.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import os
+import json
+from django.core.exceptions import ImproperlyConfigured
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+secrets = {}
+if os.path.isfile(secret_file):
+    with open(secret_file) as f:
+        secrets = json.loads(f.read())
+
+
+def get_secret(key: str, secrets: dict = secrets) -> any:
+    return secrets.get(key, None)
