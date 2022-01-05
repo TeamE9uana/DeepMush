76d79a01eb5a8f8c945a90c8a3bf7dbbcf93fed8
wsChoe <chldntjr0425@gmail.com>
2022-01-05 13:07:27 +0900

    fix: starting message to gunicorn start
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- backend/run.sh
+++ backend/run.sh
@@ -5,6 +5,6 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate --run-syncdb
 
-echo Starting Uvicorn.
+echo Starting Gunicorn.
 
 gunicorn config.wsgi:application --bind=0.0.0.0:8000 -w 8 --threads 8 -k gthread --reload
