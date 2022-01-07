3b4381868e6da67e8d49b59ec9af496314f953bb
wsChoe <chldntjr0425@gmail.com>
2022-01-07 15:47:17 +0900

    fix: modify users/me from post to get method

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,14 +2,16 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from config.models import Profile
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
 
 class MyProfileView(APIView):
-    def post(self,request):
+    @swagger_auto_schema(operation_id="Profiel 모델에서 사용자 정보 검색")
+    def get(self,request):
         """
         Profile Model의 특정 사용자 검색
         """
         ### Parsing Data ###
-        user = request.POST.get('user')
+        user = request.GET.get('user')
         result = None
         status_code = None
 
