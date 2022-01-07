0ee444719a66d776c747c4a2abe45dd14d98df0d
jiho lee <jiholee@kpu.ac.kr>
2022-01-07 16:48:51 +0900

    chore: pytorch cuda version 11.3

---
+++ backend/scripts/install-cuda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd temp
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
