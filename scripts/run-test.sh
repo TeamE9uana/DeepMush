a12cb10fbf79548876c4f3fa5b20b9f2e846017c
jiho lee <jiholee@kpu.ac.kr>
2022-01-08 12:10:23 +0900

    chore: move test scripts into backend/scripts

--- scripts/run-test.sh
+++
@@ -1,4 +0,0 @@
-SCRIPTS=$(pwd)
-BACKEND=$(cd ../backend ; pwd)
-
-docker-compose run --rm --name backend_test --volume=$SCRIPTS:/scripts --volume=$BACKEND:/backend --entrypoint="/bin/bash -c 'sleep 2s ; bash /scripts/run-test-python.sh'" backend
