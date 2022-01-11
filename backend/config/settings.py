b2288470cf75302d4a05f60c8aec386522a140d4
jiho lee <optional.int@kakao.com>
2022-01-11 11:07:48 +0900

    fix: remove staticfiles duplication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 
     'rest_framework',
     'rest_framework_simplejwt',
