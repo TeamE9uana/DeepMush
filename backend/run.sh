c245e4762b66fddb57b0e764657afedc44620bb6
jiho lee <optional.int@kakao.com>
2022-01-11 11:49:54 +0900

    feat: add collectstatic

--- backend/run.sh
+++ backend/run.sh
@@ -5,6 +5,7 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate --run-syncdb
 python3 manage.py migrate --run-syncdb --database=mongodb
+python3 manage.py collectstatic
 
 echo Starting Gunicorn.
 
