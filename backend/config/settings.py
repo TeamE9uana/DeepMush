2dbb37949b1c998cbe7933c36c290a97bae09239
jiho lee <optional.int@kakao.com>
2022-01-11 11:49:07 +0900

    fix: production static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -302,6 +302,7 @@ DATABASE_ROUTERS = ('config.dbrouters.MongoDBRouter',)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static-files/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
