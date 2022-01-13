b1904ae901ef948d08c55939f081993141cd78cf
jiho lee <optional.int@kakao.com>
2022-01-13 12:49:12 +0900

    fix: 502 script matching cni cidr

--- fix-502.sh
+++ fix-502.sh
@@ -2,8 +2,8 @@ sudo firewall-cmd --direct --permanent --remove-rules ipv4 filter FORWARD
 sudo firewall-cmd --direct --permanent --remove-rules ipv4 filter INPUT
 sudo firewall-cmd --reload
 
-sudo firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -i cni0 -s 10.62.0.0/16 -j ACCEPT
-sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.62.0.0/16 -j ACCEPT
 sudo firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -i cni0 -s 10.61.0.0/16 -j ACCEPT
-sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.61.0.0/16 -j ACCEPT
+sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.61.0.0/15 -j ACCEPT
+# sudo firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -i cni0 -s 10.62.0.0/16 -j ACCEPT
+# sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.62.0.0/16 -j ACCEPT
 sudo firewall-cmd --reload
