188261078fa780578361ee50bc1b019f05f2a0a4
jiho lee <optional.int@kakao.com>
2022-01-13 13:29:56 +0900

    fix: wrong subnet

--- fix-502.sh
+++ fix-502.sh
@@ -1,2 +1,2 @@
-sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family=ipv4 source address=10.61.10.0/16 accept"
+sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family=ipv4 source address=10.61.0.0/16 accept"
 sudo firewall-cmd --reload
