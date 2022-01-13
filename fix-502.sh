e337d9dcf1eae353d05946581223602ca6e63ade
jiho lee <optional.int@kakao.com>
2022-01-13 13:12:32 +0900

    fix: use public zone to permanent

--- fix-502.sh
+++ fix-502.sh
@@ -1,9 +1,2 @@
-sudo firewall-cmd --direct --permanent --remove-rules ipv4 filter FORWARD
-sudo firewall-cmd --direct --permanent --remove-rules ipv4 filter INPUT
-sudo firewall-cmd --reload
-
-sudo firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -i cni0 -s 10.61.0.0/16 -j ACCEPT
-sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.61.0.0/15 -j ACCEPT
-# sudo firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -i cni0 -s 10.62.0.0/16 -j ACCEPT
-# sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.62.0.0/16 -j ACCEPT
+sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family=ipv4 source address=10.61.10.0/16 accept"
 sudo firewall-cmd --reload
