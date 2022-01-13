c560431c4f0a534c51009ba23986f9fb3ffbf80c
jiho lee <optional.int@kakao.com>
2022-01-13 13:37:25 +0900

    feat: add 10.62.0.0/16

--- fix-502.sh
+++ fix-502.sh
@@ -1,2 +1,3 @@
 sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family=ipv4 source address=10.61.0.0/16 accept"
+sudo firewall-cmd --permanent --zone=public --add-rich-rule="rule family=ipv4 source address=10.62.0.0/16 accept"
 sudo firewall-cmd --reload
