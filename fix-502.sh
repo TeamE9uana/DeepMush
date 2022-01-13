01bdc333c365f1169ac314e02e66ccd1decea032
jiho lee <optional.int@kakao.com>
2022-01-13 14:41:35 +0900

    fix: add rule

--- fix-502.sh
+++ fix-502.sh
@@ -1,2 +1,2 @@
-sudo firewall-cmd --permanent --direct --delete-rule ipv4 filter FORWARD 1 -s 10.61.0.0/16 -d 10.61.0.0/16 -m state --state NEW -j ACCEPT
+sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 -s 10.61.0.0/16 -d 10.61.0.0/16 -m state --state NEW -j ACCEPT
 sudo firewall-cmd --reload
