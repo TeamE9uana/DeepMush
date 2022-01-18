9a7d32f95c2b60471e227a0cceacfa09bcb366fb
jiho lee <optional.int@kakao.com>
2022-01-18 14:21:52 +0900

    feat: init terraform script

--- deploy/setup.tf
+++ deploy/setup.tf
@@ -1,101 +1,94 @@
-#defining the provider block
-provider "aws" {
-  region = "ap-south-1"
-  profile = "default"
-}
-
-
-#aws instance creation
-resource "aws_instance" "os1" {
-    ami           = "ami-010aff33ed5991201"
-    instance_type = "t2.micro"
-    security_groups =  [ "launch-wizard-3" ]
-    key_name = "mykey1122"
-    tags = {
-        Name = "TerraformOS"
+terraform {
+  required_providers {
+    vultr = {
+      source = "vultr/vultr"
+      version = "2.8.1"
     }
-}
-
-
-#IP of aws instance retrieved
-output "op1"{
-    value = aws_instance.os1.public_ip
-}
-
-
-#IP of aws instance copied to a file ip.txt in local system
-resource "local_file" "ip" {
-    content  = aws_instance.os1.public_ip
-    filename = "ip.txt"
-}
-
-
-#ebs volume created
-resource "aws_ebs_volume" "ebs"{
-  availability_zone =  aws_instance.os1.availability_zone
-  size              = 1
-  tags = {
-    Name = "myterraebs"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 
-
-#ebs volume attatched
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.ebs.id
-  instance_id = aws_instance.os1.id
-  force_detach = true
+# Configure the Vultr Provider
+provider "vultr" {
+  api_key = var.VULTR_API_KEY
 }
 
-
-#device name of ebs volume retrieved
-output "op2"{
-value = aws_volume_attachment.ebs_att.device_name
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-2"
 }
 
-
-#connecting to the Ansible control node using SSH connection
-resource "null_resource" "nullremote1" {
-    depends_on = [aws_instance.os1] 
-    connection {
-        type     = "ssh"
-        user     = "root"
-        password = "${var.password}"
-            host= "${var.host}" 
-    }
-    #copying the ip.txt file to the Ansible control node from local system
-    provisioner "file" {
-        source      = "ip.txt"
-        destination = "/root/ansible_terraform/aws_instance/ip.txt"
-    }
+data "local_file" "vultr_pub" {
+  filename = pathexpand("~/.ssh/vultr.pub")
 }
 
-
-#connecting to the Linux OS having the Ansible playbook
-resource "null_resource" "nullremote2" {
-    depends_on = [aws_volume_attachment.ebs_att]  
-    connection {
-        type     = "ssh"
-        user     = "root"
-        password = "${var.password}"
-        host = "${var.host}"
-    }
-    #command to run ansible playbook on remote Linux OS
-    provisioner "remote-exec" {
-    
-        inline = [
-            "cd /root/ansible_terraform/aws_instance/",
-            "ansible-playbook instance.yml"
-        ]
-    }
+resource "vultr_ssh_key" "k3s_ssh_key" {
+  name = "k3s_ssh_key"
+  ssh_key = data.local_file.vultr_pub.content
 }
 
+# Create a web instance
+resource "vultr_instance" "k3s" {
+    plan = "vc2-8c-32gb"
+    region = "icn" # Korea region
+    os_id = "387" # ubuntu 20.04 x64
+    ssh_key_ids = [vultr_ssh_key.k3s_ssh_key.id]
+}
 
-# to automatically open the webpage on local system
-resource "null_resource" "nullremote3" {
-    depends_on = [null_resource.nullremote2]
+resource "null_resource" "ansible_exec" {
+    depends_on = [vultr_instance.k3s]
     provisioner "local-exec" {
-        command = "chrome http://${aws_instance.os1.public_ip}/web/"
+        command = <<EOF
+            echo "[deepmush]" > inventory
+            echo "${vultr_instance.k3s.main_ip} ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/vultr" >> inventory
+            echo "[ssh_connection]" >> inventory
+            echo "retries=999999" >> inventory
+        EOF
     }
+    provisioner "local-exec" {
+        command = <<EOT
+
+            ANSIBLE_HOST_KEY_CHECKING=False \
+            ansible-playbook -i inventory setup.yaml
+        EOT
+    }
+}
+
+resource "null_resource" "k3s_launch" {
+    depends_on = [null_resource.ansible_exec]
+    provisioner "local-exec" {
+        command = <<EOT
+            KUBECONFIG=$(pwd)/k3s.yaml:$KUBECONFIG
+            kubectl set-context deepmush-k3s
+            ../init-k3s-cluster.sh
+        EOT
+    }
+}
+
+resource "aws_route53_zone" "primary" {
+    depends_on = [null_resource.k3s_launch]
+    name = "deepmush.io"
+}
+
+resource "aws_route53_record" "root" {
+    depends_on = [aws_route53_zone.primary]
+
+    zone_id = aws_route53_zone.primary.zone_id
+    name    = "deepmush.io"
+    type    = "A"
+    ttl     = "60"
+    records = [vultr_instance.k3s.main_ip]
+}
+
+resource "aws_route53_record" "sub" {
+    depends_on = [aws_route53_zone.primary]
+
+    zone_id = aws_route53_zone.primary.zone_id
+    name    = "*.deepmush.io"
+    type    = "A"
+    ttl     = "60"
+    records = [vultr_instance.k3s.main_ip]
 }
---
+++ deploy/variables.tf
@@ -0,0 +1,5 @@
+variable "VULTR_API_KEY" {
+  description = "vultr api key"
+  type        = string
+  sensitive = true
+}
