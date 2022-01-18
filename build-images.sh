f753707c1d959365d561906935222f26fc7a514f
jiho lee <optional.int@kakao.com>
2022-01-18 11:52:05 +0900

    refactor: use filebeat base image only

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 docker build -t deepmush_backend ./backend
-docker build -t deepmush_filebeat ./filebeat
--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 docker tag deepmush_backend:latest public.ecr.aws/w1d0u6d8/deepmush_backend:latest
-docker tag deepmush_filebeat:latest public.ecr.aws/w1d0u6d8/deepmush_filebeat:latest
 
 docker push public.ecr.aws/w1d0u6d8/deepmush_backend:latest
-docker push public.ecr.aws/w1d0u6d8/deepmush_filebeat:latest
