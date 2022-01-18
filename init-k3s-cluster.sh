badcb24239675b0adac2e036b131b824a852eb6a
jiho lee <optional.int@kakao.com>
2022-01-18 11:28:28 +0900

    fix: pvc

--- init-k3s-cluster.sh
+++ init-k3s-cluster.sh
@@ -1,5 +1,6 @@
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 ./install-cert-manager.sh
+kubectl apply -f ./k8s/local-path-provisioner
 kubectl apply -f ./k8s/volumes
 kubectl apply -f ./k8s/auth
 kubectl apply -f ./k8s/ingress
